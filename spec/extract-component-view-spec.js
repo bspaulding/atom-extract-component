a81ed65c3be34556df8bf8bcb92b5c9e23b89e23
Bradley Spaulding <brad.spaulding@gmail.com>
2016-08-23 13:34:43 -0400

    Initial commit

---
+++ spec/extract-component-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ExtractComponentView from '../lib/extract-component-view';
+
+describe('ExtractComponentView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
